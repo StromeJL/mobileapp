59fc36bce99ad89a6bdb0c89360d545b85221bd3
Juan Ignacio Laube <juanlaube@icloud.com>
2018-06-12 14:03:28 -0300

    :art: Polish inaccessible project display

---
+++ Toggl.Foundation/Extensions/ThreadSafeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Toggl.Foundation.Models.Interfaces;
+
+namespace Toggl.Foundation.Extensions
+{
+    public static class ThreadSafeExtensions
+    {
+        private const string archivedColor = "#cecece";
+
+        public static string DisplayName(this IThreadSafeProject project)
+        {
+            var name = project.Name ?? "";
+
+            if (name == Resources.InaccessibleProject)
+            {
+                return name;
+            }
+
+            return project.Active ? name : $"{name} (archived)";
+        }
+
+        public static string DisplayColor(this IThreadSafeProject project)
+        {
+            if (project.Name == Resources.InaccessibleProject)
+            {
+                return archivedColor;
+            }
+            return project.Color ?? "";
+        }
+    }
+}
